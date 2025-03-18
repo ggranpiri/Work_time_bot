5ec4c5d6cc5d715d6c6b7128e2fbb9dadfc997bf
София Совкова <sdsovkova@edu.hse.ru>
2025-03-18 22:53:05 +0300

    добавила сообщение о запуске бота, дефолтный обед, обработка ошибок

--- bot/main.py
+++ bot/main.py
@@ -5,7 +5,7 @@ from aiogram.filters import Command, StateFilter
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from config import TOKEN, ADMINS_ID
-from work_with_sheets import log_event, calculate_work_time, add_user, get_balance, update_balance, get_all_balances
+from work_with_sheets import *
 from buttons import worker_kb, admin_kb, get_employee_keyboard, PayCallback
 
 # Логирование
@@ -71,7 +71,7 @@ async def choose_employee(message: types.Message, state: FSMContext):
 async def enter_salary_amount(call: types.CallbackQuery, state: FSMContext, callback_data: dict):
     """Получаем ID выбранного сотрудника и запрашиваем сумму"""
     user_id = callback_data.user_id  # Получаем ID сотрудника
-    name = callback_data.name
+    name = get_user_name(user_id)
     await state.update_data(user_id=user_id)
 
     await call.message.answer(f"Введите сумму для выплаты {name}:")
@@ -83,11 +83,12 @@ async def process_salary_payment(message: types.Message, state: FSMContext):
     """Обрабатываем выплату зарплаты"""
     data = await state.get_data()
     user_id = data["user_id"]  # Получаем ID сотрудника
+    name = get_user_name(user_id)
     amount = float(message.text)
 
     new_balance = update_balance(user_id, -amount)
     if new_balance is not None:
-        await message.answer(f"Сотруднику выплачено {amount} руб.\nНовый баланс: {new_balance} руб.")
+        await message.answer(f"Сотруднику {name} выплачено {amount} руб.\nНовый баланс: {new_balance} руб.")
         # Отправляем уведомление сотруднику
         try:
             await bot.send_message(user_id, f"Вам выплачено {amount} руб.\nНовый баланс: {new_balance} руб.")
@@ -103,28 +104,37 @@ async def process_salary_payment(message: types.Message, state: FSMContext):
 @dp.message(lambda message: message.text in ["Приход", "Уход", "Начал обед", "Закончил обед"])
 async def worker_action(message: types.Message):
     user_id = message.from_user.id
-    name = message.from_user.full_name
+    name = get_user_name(user_id)
     action = message.text
+    try:
+        # Проверяем и исправляем предыдущее событие
+        fix_message = check_and_fix_records(user_id, name, action)
+        if fix_message:
+            await message.answer(fix_message)
 
-    if action == "Уход":
-        work_hours, salary = calculate_work_time(user_id)
-        new_balance = update_balance(user_id, salary)
-        log_event(user_id, name, action, work_hours, salary)
-        await message.answer(f"Вы отработали {work_hours} часов и заработали {salary} руб.\n"
-                             f"Ваш текущий баланс: {new_balance} руб.")
+        if action == "Уход":
+            work_hours, salary = calculate_work_time(user_id)
+            new_balance = update_balance(user_id, salary)
+            log_event(user_id, name, action, work_hours=work_hours, salary=salary)
+            await message.answer(f"Вы отработали {work_hours} часов и заработали {salary} руб.\n"
+                                 f"Ваш текущий баланс: {new_balance} руб.")
 
-        # Уведомляем всех администраторов
-        await notify_admins(f"Сотрудник {name} ушёл с работы.\n"
-                            f"Отработано: {work_hours} часов\n"
-                            f"Зарплата: {salary} руб.\n"
-                            f"Текущий баланс: {new_balance} руб.")
-    else:
-        log_event(message.from_user.id, message.from_user.full_name, message.text)
-        await message.answer(f"Записано: {message.text}")
-        await notify_admins(f"Сотрудник {name} отметил: {action}")
+            # Уведомляем всех администраторов
+            await notify_admins(f"Сотрудник {name} ушёл с работы.\n"
+                                f"Отработано: {work_hours} часов\n"
+                                f"Зарплата: {salary} руб.\n"
+                                f"Текущий баланс: {new_balance} руб.")
+        else:
+            log_event(user_id, name, action)
+            await message.answer(f"Записано: {action}")
+            await notify_admins(f"Сотрудник {name} отметил: {action}")
+    except Exception as e:
+        print(e)
+        await bot.send_message(user_id, f"Не получилось выполнить действие. Ошибка: {e}")
 
 
 async def main():
+    await notify_admins("Бот запущен")
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
--- bot/work_with_sheets.py
+++ bot/work_with_sheets.py
@@ -1,6 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from config import SPREADSHEET_NAME, DEFAULT_HOURLY_RATE
+from config import SPREADSHEET_NAME, DEFAULT_HOURLY_RATE, DEFAULT_LUNCH_TIME, AUTO_CHECK_OUT_TIME, AUTO_CHECK_IN_TIME
 from datetime import datetime, timedelta
 
 # Авторизация в Google Sheets
@@ -14,21 +14,131 @@ sheet = client.open(SPREADSHEET_NAME).sheet1  # Лист для событий
 sheet_accounts = spreadsheet.worksheet("Счета")  # Лист для счетов
 
 
-def log_event(user_id, name, event, work_hours=None, salary=None):
+def log_event(user_id, name, event, time=None, work_hours=None, salary=None):
     """Записывает событие в таблицу. Если переданы work_hours и salary — записывает итоговый отчёт по смене."""
-    timestamp = datetime.now().strftime("%d-%m-%Y %H:%M:%S")  # Получаем текущее время
+    if not time:
+        time = datetime.now()  # Получаем текущее время
+    time = time.strftime("%d-%m-%Y %H:%M:%S")
     if work_hours is None and salary is None:
         # Обычное событие (Приход, Начал обед, Закончил обед)
-        sheet.append_row([timestamp, user_id, name, event])
+        sheet.append_row([time, user_id, name, event])
     else:
         # Специальная запись для "Уход"
-        sheet.append_row([timestamp, user_id, name, "Уход", f"{work_hours} ч", f"{salary} руб."])
+        sheet.append_row([time, user_id, name, "Уход", f"{work_hours} ч", f"{salary} руб."])
+
+def get_user_name(user_id):
+    """Возвращает имя сотрудника по user_id со второго листа таблицы."""
+    records = sheet_accounts.get_all_values()  # Загружаем данные второго листа
+
+    for row in records:
+        if row[0] == str(user_id):  # Первый столбец — user_id
+            return row[1]  # Второй столбец — имя
+
+    return None  # Если сотрудник не найден
+
+def check_and_fix_records(user_id, user_name, event):
+    """Проверяет последнее событие и автоматически добавляет недостающие записи"""
+    records = sheet.get_all_values()
+    last_event = None
+    last_event_type = None
+
+    for row in reversed(records):
+        if row[1] == str(user_id):
+            last_event = datetime.strptime(row[0], "%d-%m-%Y %H:%M:%S")
+            last_event_type = row[3]
+            break
+
+    messages = []
+    if event == "Приход" and last_event_type != "Уход":
+        if last_event.date() < datetime.now().date():
+            # Если приход был в прошедший день → закрываем смену в 19:00
+            auto_checkout_time = datetime.combine(
+                last_event.date(), datetime.strptime(AUTO_CHECK_OUT_TIME, "%H:%M:%S").time()
+            )
+            log_event(user_id, user_name, "Уход", auto_checkout_time)
+            messages.append(
+                f"Смена за {last_event.strftime('%d-%m-%Y')} автоматически закрыта в {AUTO_CHECK_OUT_TIME}.")
+
+        elif last_event.date() == datetime.now().date():
+            # Если приход был сегодня → закрываем смену сейчас и сразу добавляем новый приход
+            current_time = datetime.now()
+            log_event(user_id, user_name, "Уход", current_time)
+
+            messages.append(f"Смена закрыта в {current_time.strftime('%H:%M')} и сразу начата новая смена.")
+
+
+
+    elif event == "Уход" and last_event_type not in ["Закончил обед"]:
+        # Если последнее событие — "Начал обед", но нет "Закончил обед" → добавляем "Закончил обед"
+        if last_event_type == "Начал обед":
+            time = datetime.now()
+            log_event(user_id, user_name, "Закончил обед", time)
+            messages.append(f"Автоматически закончен обед в {time.strftime('%H:%M')}")
+
+        if last_event_type == "Уход":
+            if last_event.date() != datetime.now().date():
+                auto_checkin_time = datetime.combine(datetime.now().date(),
+                                                     datetime.strptime(AUTO_CHECK_IN_TIME, "%H:%M:%S").time())
+
+                log_event(user_id, user_name, "Приход", auto_checkin_time)
+                messages.append(f"Автоматически открыта смена за {last_event.strftime('%d-%m-%Y')}")
+            else:
+                current_time = datetime.now()
+                log_event(user_id, user_name, "Приход", current_time)
+                messages.append(f"Смена автоматически открыта в {current_time.strftime('%H:%M')}")
+        if last_event_type == "Приход":
+            if last_event.date() != datetime.now().date():
+                auto_checkin_time = datetime.combine(datetime.now().date(),
+                                                     datetime.strptime(AUTO_CHECK_IN_TIME, "%H:%M:%S").time())
+                auto_checkout_time = datetime.combine(last_event.date(),
+                                                     datetime.strptime(AUTO_CHECK_OUT_TIME, "%H:%M:%S").time())
+                log_event(user_id, user_name, "Уход", auto_checkout_time)
+                log_event(user_id, user_name, "Приход", auto_checkin_time)
+                messages.append(f"Автоматически закрыта смена за {last_event.strftime('%d-%m-%Y')} и открыта за сегодня")
+
+
+
+    elif event in "Начал обед" and last_event_type not in ["Закончил обед", "Приход"]:
+        # Если последнее событие — "Начал обед", но нет "Закончил обед" → добавляем "Закончил обед"
+        if last_event_type == "Начал обед":
+            time = datetime.now() + timedelta(seconds=DEFAULT_LUNCH_TIME)
+            log_event(user_id, user_name, "Закончил обед", time)
+            messages.append(f"Автоматически закончен обед в {time.strftime('%H:%M')}.")
+
+        if last_event_type == "Уход":
+            if last_event.date() != datetime.now().date():
+                # Если обед начался в другой день, открываем смену и фиксируем обед
+                auto_checkin_time = datetime.combine(datetime.now().date(),
+                                                     datetime.strptime(AUTO_CHECK_IN_TIME, "%H:%M:%S").time())
+
+                log_event(user_id, user_name, "Приход", auto_checkin_time)
+                messages.append(f"Автоматически открыта смена за {datetime.now().strftime('%d-%m-%Y')} и начат обед в {datetime.now().strftime('%H:%M')}.")
+            else:
+                current_time = datetime.now()
+                log_event(user_id, user_name, "Приход", current_time)
+                messages.append(f"Смена автоматически открыта в {current_time.strftime('%H:%M')}")
+
+    elif event == "Закончил обед" and last_event_type not in ["Начал обед"]:
+        min_lunch_start_time = datetime.now() - timedelta(seconds=DEFAULT_LUNCH_TIME)
+
+        for row in reversed(records):
+            if row[1] == str(user_id) and row[3] == "Приход":
+                last_check_in = datetime.strptime(row[0], "%d-%m-%Y %H:%M:%S")
+                min_lunch_start_time = min(last_check_in, min_lunch_start_time)
+                break
+
+        log_event(user_id, user_name, "Начал обед", min_lunch_start_time)
+        messages.append(f"Автоматически добавлено 'Начал обед' в {min_lunch_start_time.strftime('%H:%M')}.")
+
+    return "\n".join(messages) if messages else None
 
 
 def calculate_work_time(user_id):
     """Вычисляет отработанное время и зарплату"""
     records = sheet.get_all_values()  # Загружаем все записи
     last_check_in = None
+    last_check_out = None
+    lunch_start = None
     lunch_end = None
     total_lunch_time = 0  # Время обеда в секундах
 
@@ -39,29 +149,42 @@ def calculate_work_time(user_id):
             if row[3] == "Приход":
                 last_check_in = time_event
                 break
+            elif row[3] == "Уход":
+                last_check_out = time_event
+                break
             elif row[3] == "Закончил обед":
                 lunch_end = time_event
-            elif row[3] == "Начал обед" and lunch_end:
-                total_lunch_time += (lunch_end - time_event).total_seconds()
-                lunch_end = None
+            elif row[3] == "Начал обед":
+                lunch_start = time_event
+                if lunch_end:
+                    total_lunch_time += (lunch_end - lunch_start).total_seconds()
+                    lunch_end = None
+                    lunch_start = None
 
-    if last_check_in:
-        now = datetime.now()
-        work_time = now - last_check_in - timedelta(seconds=total_lunch_time)
-        work_hours = round(work_time.total_seconds() / 3600, 2)  # Время в часах
 
-        # Получаем почасовую ставку сотрудника
-        accounts = sheet_accounts.get_all_values()
-        hourly_rate = DEFAULT_HOURLY_RATE  # Значение по умолчанию
+    if lunch_start:
+        total_lunch_time += DEFAULT_LUNCH_TIME
+    if lunch_end:
+        total_lunch_time += DEFAULT_LUNCH_TIME
 
-        for row in accounts:
-            if row[0] == str(user_id):  # Найден сотрудник
-                hourly_rate = float(row[2])  # Берём ставку из таблицы
-                break
+    work_time = datetime.now() - last_check_in - timedelta(seconds=total_lunch_time)
 
-        salary = round(work_hours * hourly_rate, 2)  # Заработок
-        return work_hours, salary
-    return 0, 0
+    if work_time >= timedelta(hours=8) and not total_lunch_time:
+        work_time -= timedelta(seconds=DEFAULT_LUNCH_TIME)
+
+    work_hours = round(work_time.total_seconds() / 3600, 2)  # Время в часах
+
+    # Получаем почасовую ставку сотрудника
+    accounts = sheet_accounts.get_all_values()
+    hourly_rate = DEFAULT_HOURLY_RATE  # Значение по умолчанию
+
+    for row in accounts:
+        if row[0] == str(user_id):  # Найден сотрудник
+            hourly_rate = float(row[2])  # Берём ставку из таблицы
+            break
+
+    salary = round(work_hours * hourly_rate, 2)  # Заработок
+    return work_hours, salary
 
 
 def add_user(user_id, name):
@@ -99,13 +222,12 @@ def update_balance(user_id, amount):
             return new_balance
     return None  # Если сотрудника нет
 
+
 def get_all_balances():
     """Возвращает список сотрудников с их балансами"""
     accounts = sheet_accounts.get_all_values()[1:]  # Пропускаем заголовки
     return [[row[1], row[3]] for row in accounts]
 
+
 def get_all_accounts():
     return sheet_accounts.get_all_values()[1:]
-
-
-
